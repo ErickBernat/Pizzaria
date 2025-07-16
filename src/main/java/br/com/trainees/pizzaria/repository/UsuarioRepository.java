7ede1c1f1ed732dfa22db39bd07cc157ba5f6577
monique silva <moniquesilva@almavivasolutions.com.br>
2025-07-16 12:17:04 -0300

    feat: Incluindo querys de consulta por endereço, alteração de senha e mudança de status

--- src/main/java/br/com/trainees/pizzaria/repository/UsuarioRepository.java
+++ src/main/java/br/com/trainees/pizzaria/repository/UsuarioRepository.java
@@ -1,30 +1,53 @@
 package br.com.trainees.pizzaria.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import br.com.trainees.pizzaria.domain.entity.Usuario;
 
-
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	@Query("SELECT u FROM Usuario u"
-			+ "LEFT JOIN u.")
+			+ "LEFT JOIN u.endereco "
+			+ "WHERE u.id = :id")
 	Optional<Usuario> buscaUsuarioPorId(@Param("id") Long id);
 	
-	Optional<Usuario> buscaUsuarioPorCpf(String cpf);
+	@Query("SELECT u FROM Usuario u " +
+		     "LEFT JOIN FETCH u.endereco " +
+		     "WHERE u.cpf = :cpf")
+	Optional<Usuario> buscaUsuarioPorCpf(@Param("cpf") String cpf);
 	
+    @Modifying
+    @Transactional
 	@Query("UPDATE Usuario u SET u.email= :novoEmail "
 			+ "WHERE u.id = :id ")
-	Optional<Usuario> mudaEmailUsuario(@Param("novoEmail") String email, @Param("id") Long id);
+	void mudaEmailUsuario(@Param("novoEmail") String email, @Param("id") Long id);
 	
-	Optional<Usuario> mudaSenhaUsuario();
+    @Modifying
+    @Transactional
+    @Query("UPDATE Usuario u SET u.senha = :novaSenha " + 
+            "WHERE u.id = :id")
+    void mudaSenhaUsuario(@Param("novaSenha") String senha, @Param("id") Long id);
 
-	Optional<Usuario> buscaUsuarioPorEndereco();
+
+    @Query("SELECT u FROM Usuario u " +
+    	    "JOIN u.endereco e " +
+    	    "WHERE e.bairro = :bairro")
+    List<Usuario> buscaUsuarioPorBairro(@Param("bairro") String bairro);
+    
+    @Modifying
+    @Transactional
+    @Query("UPDATE Usuario u SET u.status = 0 " //
+            + "WHERE u.id = :id")
+    void deixaUsuarioInativo(@Param("id") Long id);
+
 	
 }
