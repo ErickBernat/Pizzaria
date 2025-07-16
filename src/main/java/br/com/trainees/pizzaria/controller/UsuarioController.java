7f8b704e60a5625c0d227e4c2ec849a3789fa827
Laura Paradisi <lparadisi@almavivasolution.com.br>
2025-07-16 14:49:46 -0300

    feat: adicao de controller para buscar usuarios por cpf

--- src/main/java/br/com/trainees/pizzaria/controller/UsuarioController.java
+++ src/main/java/br/com/trainees/pizzaria/controller/UsuarioController.java
@@ -1,10 +1,23 @@
 package br.com.trainees.pizzaria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.trainees.pizzaria.domain.dto.UsuarioDTO;
+import br.com.trainees.pizzaria.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-
+	
+	@Autowired
+	private UsuarioService service;
+	
+	@GetMapping("/{cpf}")
+	public UsuarioDTO obterUsuarioPorCpf(@PathVariable String cpf) {
+		return service.buscarUsuarioPorCpf(cpf);
+	}
 }
