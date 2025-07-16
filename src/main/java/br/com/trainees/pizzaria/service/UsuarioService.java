fc8f5d430e06b67ec37044882ac1f6831519d7f8
Erick <ebernat@almavivasolutions.com.br>
2025-07-16 12:19:50 -0300

    Feat:Adicionando metodo para buscar todos os usuarios

--- src/main/java/br/com/trainees/pizzaria/service/UsuarioService.java
+++ src/main/java/br/com/trainees/pizzaria/service/UsuarioService.java
@@ -1,8 +1,23 @@
 package br.com.trainees.pizzaria.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.trainees.pizzaria.domain.dto.UsuarioDTO;
+import br.com.trainees.pizzaria.domain.entity.Usuario;
+import br.com.trainees.pizzaria.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public List<UsuarioDTO> buscaTodosUsuarios(){
+		return usuarioRepository.findAll().stream().map(()-> new Usuario(this)).toList();
+	}
+	
 
 }
