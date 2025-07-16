0c8ccb96769baf6cafdcde3f8436332a2292ecea
Laura Paradisi <lparadisi@almavivasolution.com.br>
2025-07-16 15:26:12 -0300

    feat: adicao de controller de nusca de usuario por cpf

--- src/main/java/br/com/trainees/pizzaria/controller/UsuarioController.java
+++ src/main/java/br/com/trainees/pizzaria/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.trainees.pizzaria.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class UsuarioController {
 	private UsuarioService service;
 	
 	@GetMapping("/{cpf}")
-	public UsuarioDTO obterUsuarioPorCpf(@PathVariable String cpf) {
-		return service.buscarUsuarioPorCpf(cpf);
+	public ResponseEntity<UsuarioDTO> obterUsuarioPorCpf(@PathVariable String cpf) {
+		return ResponseEntity.ok(service.buscarUsuarioPorCpf(cpf));
 	}
 }
