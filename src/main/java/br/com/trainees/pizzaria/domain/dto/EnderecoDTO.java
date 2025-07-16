7f9c0da02dbdeaf6ec54de31ff19ed6c2d334a8a
Laura Paradisi <lparadisi@almavivasolution.com.br>
2025-07-16 16:30:33 -0300

    feat: adicao de endereco dto

--- src/main/java/br/com/trainees/pizzaria/domain/dto/EnderecoDTO.java
+++ src/main/java/br/com/trainees/pizzaria/domain/dto/EnderecoDTO.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record EnderecoDTO(
 		@JsonProperty(access = JsonProperty.Access.READ_ONLY) Long id,
+		String logradouro,
 		@NotBlank String cep,
 		String bairo,
 		String complemento) {
