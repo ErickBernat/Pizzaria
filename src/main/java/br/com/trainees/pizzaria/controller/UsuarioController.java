02ac02176275abd575496866c454ef8abaadb101
Laura Paradisi <lparadisi@almavivasolution.com.br>
2025-07-17 09:13:10 -0300

    fix: remocao de imports nao utilizados

--- src/main/java/br/com/trainees/pizzaria/controller/UsuarioController.java
+++ src/main/java/br/com/trainees/pizzaria/controller/UsuarioController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.trainees.pizzaria.domain.dto.UsuarioAtualizaDTO;
@@ -62,7 +60,7 @@ public class UsuarioController {
 	public ResponseEntity<UsuarioDTO> atualizaUsuario(@PathVariable Long id , @RequestBody UsuarioAtualizaDTO usuarioAtualizaDTO){
 		UsuarioDTO usuarioAtualizado = usuarioService.atualizarUsuario(id, usuarioAtualizaDTO);
 		return ResponseEntity.ok(usuarioAtualizado);
-
+	}
 	@PatchMapping("/{id}/inativar")
 	public ResponseEntity<?> inativarUsuario(@PathVariable("id") Long usuarioId) {
 		usuarioService.deletarUsuario(usuarioId);
